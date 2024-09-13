7c2ac2cdf3928cc4d714fe02b40b339a57259fb4
dummy <94170289@qq.com>
2024-09-13 21:16:44 +0800

    update

--- dictation.py
+++ dictation.py
@@ -1,6 +1,6 @@
 import pyttsx3
 
-CNT = 3
+CNT = 10
 
 if __name__ == '__main__':
     # 初始化TTS引擎
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     # 设置语速，可选，可以根据需要调整
     # 值越小语速越慢，值越大语速越快
     rate = engine.getProperty('rate')
-    engine.setProperty('rate', rate - 200)
+    engine.setProperty('rate', rate - 50)
 
     # 将文本添加到TTS引擎
     for text in texts:
