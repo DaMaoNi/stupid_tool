ca2bb8b00a850360048a215e457240045be46f3c
大猫鲵 <dummy@damaonideMacBook-Pro.local>
2024-10-03 20:40:49 +0800

    update

--- stock.py
+++ stock.py
@@ -65,7 +65,9 @@ def judge(tmp_code):
 
 
 if __name__ == '__main__':
-    stock_codes = efinance.stock.get_all_company_performance().get('股票代码')
+    stock_codes = []
+    if len(stock_codes) == 0:
+        stock_codes = efinance.stock.get_all_company_performance().get('股票代码')
     for stock_code in stock_codes:
         judge(stock_code)
     print('done!')
