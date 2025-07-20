e428af46dfc980ce2083169c801c1a3b574ca42e
dummy <94170289@qq.com>
2025-07-20 15:34:14 +0800

    update

--- iptv.py
+++ iptv.py
@@ -10,18 +10,15 @@ if __name__ == "__main__":
         data = json.load(f)
     total = len(data)
     i = 0
-    with open('iptv_result.txt', 'w', encoding='utf-8') as f:
-        for item in data:
-            try:
-                url = item["URL"]
-                iptv_url = f'{url}{TEST_URL}'
-                i += 1
-                print(f'{i}/{total}')
-                stream_response = requests.get(iptv_url, stream=True, timeout=3)
-                chunk = next(stream_response.iter_content(chunk_size=1024))
-                if chunk:
-                    print(iptv_url)
-                    f.write(iptv_url)
-                    f.write('\n')
-            except:
-                pass
+    for item in data:
+        try:
+            url = item["URL"]
+            iptv_url = f'{url}{TEST_URL}'
+            i += 1
+            print(f'{i}/{total}')
+            stream_response = requests.get(iptv_url, stream=True, timeout=3)
+            chunk = next(stream_response.iter_content(chunk_size=1024))
+            if chunk:
+                print(iptv_url)
+        except:
+            pass
